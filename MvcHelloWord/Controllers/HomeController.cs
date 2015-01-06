0a77c909a3a90adb8532340d8e3bc212beb93613
lipanpan <keep_running2@163.com>
2015-01-06 16:16:02 +0800

    mvchelloword
    测试应用程序

---
+++ MvcHelloWord/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MvcHelloWord.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHelloWord.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int next = new Random().Next(1, 100);
+
+            ViewBag.index = next < 40 ? "hello niuniu" : "hello lyce";
+            return View();
+        }
+
+
+        public string SendInforomation(Person person)
+        {
+            return "Thanks you " + person.Name;
+        }
+
+    }
+}
